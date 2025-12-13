593f64833ae241debb3a4ba0e111e308e97d1005
lucy <wanjikul598@gmail.com>
2025-12-14 00:30:00 +0300

    git commit remove hardcoded SMTP credentials and load email settings from environment variables

--- backend/stockhive_backend/settings.py
+++ backend/stockhive_backend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -172,8 +176,12 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "wanjikul598@gmail.com"
-EMAIL_HOST_PASSWORD = "dgly sejh ihaw hnto "
+
+import os
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 
 DEFAULT_FROM_EMAIL = "StockHive <yourgmail@gmail.com>"
 
